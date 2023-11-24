f16df51de9d50042c1a140fd480589281098beee
Bravco <boss.wiw@gmail.com>
2023-11-24 09:49:03 +0100

    Added simple-robots

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,12 @@ export default defineNuxtConfig({
   modules: [
     "nuxt-icon",
     "@nuxt/image",
+    "nuxt-simple-robots",
   ],
   css: [
     "@/assets/main.css",
   ],
+  site: {
+    url: process.env.PUBLIC_SITE_URL,
+  },
 });
